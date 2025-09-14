b55cc2dec4ec422c99dc386499fab4c72e8ea2eb
Hld22 <haukeheld@gmx.de>
2025-09-14 12:17:39 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -6,22 +6,23 @@ const clock = document.getElementById('clock')
 var uploaded_image = "";
 
 function getTime() {
-    const now = new Date();
-    const seconds = now.getSeconds();
-    const minutes = now.getMinutes();
-    const hours = now.getHours();
-    const timeInterval = 6;
+    const now= new Date()
+    const seconds = now.getSeconds()
+    const minutes = now.getMinutes()
+    const hours = now.getHours()
+    const timeInterval = 6
+//    console.log(seconds)
+    secondsHand.style.transform = 'rotate('+ (seconds * timeInterval) + 'deg)'
+    minutesHand.style.transform = 'rotate('+ (minutes * timeInterval + seconds / 10) + 'deg)'
+    hoursHand.style.transform = 'rotate('+ (hours * 30 + minutes / 2) + 'deg)'
 
-    secondsHand.style.transform = 'translate(-50%, 0) rotate(' + (seconds * timeInterval) + 'deg)';
-    minutesHand.style.transform = 'translate(-50%, 0) rotate(' + (minutes * timeInterval + seconds / 10) + 'deg)';
-    hoursHand.style.transform = 'translate(-50%, 0) rotate(' + ((hours % 12) * 30 + minutes * 0.5) + 'deg)';
-
-    if ((hours === 12 && minutes >= 33) || (hours === 13 && minutes <= 15)) {
+    if (
+        (hours == 12 && minutes >= 33) ||
+        (hours == 13 && minutes <= 15)) {
         clock.style.backgroundImage = 'url("LernzeitUhr.png")';
-        clock.style.backgroundSize = 'cover';
     } else {
         clock.style.backgroundImage = '';
     }
 }
-setInterval(getTime, 1000);
-getTime();
+setInterval(getTime, 500)
+getTime()
